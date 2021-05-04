829106e530121935dcbcbd5b184114540836d8eb
Max Hora <maxhora.ua@gmail.com>
2021-05-05 00:04:15 +0300

    Adjust test fmt output.

--- dupe-detection/correlation_test.go
+++ dupe-detection/correlation_test.go
@@ -171,9 +171,11 @@ func TestCollerations(t *testing.T) {
 			result := compute_randomized_dependence_func(inputs1[i], inputs2[i])
 			if result != outputs[i] {
 				differentsCount++
-				t.Errorf("RDC Calculated correlation doesn't match the expected result (diff is %v):\n%v\n%v", math.Abs(result-outputs[i]), result, outputs[i])
+				//t.Errorf("RDC Calculated correlation doesn't match the expected result (diff is %v):\n%v\n%v", math.Abs(result-outputs[i]), result, outputs[i])
+				fmt.Printf("\n%v", result)
 			}
 		}
+		fmt.Printf("\n--------------------------------")
 	}
 	fmt.Printf("\nRDC - Found calculation differences: %v from total number of %v with floating point accuracy %v", differentsCount, len(inputs1), kendallFloatingPointAccuracy)
 
