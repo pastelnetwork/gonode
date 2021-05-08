9c8d4534ba0cd64332726275b9628176a0c7c4d0
Max Hora <maxhora.ua@gmail.com>
2021-05-08 20:04:55 +0300

    Configure goptuna study with all correlations thresholds.

--- dupe-detection/cmd/optimizer/optimizer.go
+++ dupe-detection/cmd/optimizer/optimizer.go
@@ -12,22 +12,27 @@ import (
 	"github.com/pastelnetwork/gonode/dupe-detection/pkg/dupedetection"
 )
 
-// ① Define an objective function which returns a value you want to minimize.
+const EvaluateNumberOfTimes = 3
+
+// objective defines the objective of the study - find out the best aurpc value
 func objective(trial goptuna.Trial) (float64, error) {
-	// ② Define the search space via Suggest APIs.
+	// Define the search space via Suggest APIs.
 	config := dupedetection.NewComputeConfig()
 	config.PearsonDupeThreshold, _ = trial.SuggestFloat("PearsonDupeThreshold", 0.5, 0.99999)
 	config.SpearmanDupeThreshold, _ = trial.SuggestFloat("SpearmanDupeThreshold", 0.5, 0.99999)
+	config.KendallDupeThreshold, _ = trial.SuggestFloat("KendallDupeThreshold", 0.5, 0.99999)
+	config.RandomizedDependenceDupeThreshold, _ = trial.SuggestFloat("RandomizedDependenceDupeThreshold", 0.5, 0.99999)
+	config.RandomizedBlomqvistDupeThreshold, _ = trial.SuggestFloat("RandomizedBlomqvistDupeThreshold", 0.5, 0.99999)
+	config.HoeffdingDupeThreshold, _ = trial.SuggestFloat("HoeffdingDupeThreshold", 0.1, 0.99999)
+	config.HoeffdingRound2DupeThreshold, _ = trial.SuggestFloat("HoeffdingRound2DupeThreshold", 0.1, 0.99999)
 
 	aurpc := 1.0 - auprc.MeasureAUPRC(config)
 	return aurpc, nil
 }
 
 func main() {
-
 	rand.Seed(time.Now().UnixNano())
 
-	// ③ Create a study which manages each experiment.
 	study, err := goptuna.CreateStudy(
 		"goptuna-aurpc",
 		goptuna.StudyOptionRelativeSampler(cmaes.NewSampler()))
@@ -35,15 +40,21 @@ func main() {
 		panic(err)
 	}
 
-	// ④ Evaluate your objective function.
-	err = study.Optimize(objective, 25)
+	// Evaluate objective function specified number of times
+	err = study.Optimize(objective, EvaluateNumberOfTimes)
 	if err != nil {
 		panic(err)
 	}
 
-	// ⑤ Print the best evaluation parameters.
 	v, _ := study.GetBestValue()
 	p, _ := study.GetBestParams()
-	log.Printf("Best value=%f (PearsonDupeThreshold=%f, SpearmanDupeThreshold=%f)",
-		v, p["PearsonDupeThreshold"].(float64), p["SpearmanDupeThreshold"].(float64))
+	log.Printf("Best value=%f \nPearsonDupeThreshold=%f,\nSpearmanDupeThreshold=%f\nKendallDupeThreshold=%f\nRandomizedDependenceDupeThreshold=%f\nRandomizedBlomqvistDupeThreshold=%f\nHoeffdingDupeThreshold=%f\nHoeffdingRound2DupeThreshold=%f",
+		v,
+		p["PearsonDupeThreshold"].(float64),
+		p["SpearmanDupeThreshold"].(float64),
+		p["KendallDupeThreshold"].(float64),
+		p["RandomizedDependenceDupeThreshold"].(float64),
+		p["RandomizedBlomqvistDupeThreshold"].(float64),
+		p["HoeffdingDupeThreshold"].(float64),
+		p["HoeffdingRound2DupeThreshold"].(float64))
 }
