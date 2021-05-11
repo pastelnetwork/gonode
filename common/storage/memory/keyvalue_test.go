85f452d32f857756e5cac687a80a1f199098f883
ashadi <ashadi.cc@gmail.com>
2021-05-11 22:01:55 +0700

    fix: rename the memory file prefixes, handle data race test case

--- common/storage/memory/keyvalue_test.go
+++ common/storage/memory/keyvalue_test.go
@@ -8,6 +8,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func NewTestDB() *keyValue {
+	return &keyValue{
+		values: map[string][]byte{
+			"exist": []byte("bar"),
+		},
+	}
+}
+
 func TestGet(t *testing.T) {
 	t.Parallel()
 
@@ -19,7 +27,7 @@ func TestGet(t *testing.T) {
 		{
 			key:           "exist",
 			expectedError: nil,
-			expectedValue: []byte("value-exist"),
+			expectedValue: []byte("bar"),
 		}, {
 			key:           "not-exist",
 			expectedError: storage.ErrKeyNotFound,
@@ -28,18 +36,13 @@ func TestGet(t *testing.T) {
 	}
 
 	t.Run("group", func(t *testing.T) {
-		db := &keyValue{
-			values: map[string][]byte{
-				"exist": []byte("value-exist"),
-			},
-		}
-
 		for _, testCase := range testCases {
+
 			testCase := testCase
 			testName := fmt.Sprintf("key:%s/value:%v/err:%v", testCase.key, testCase.expectedValue, testCase.expectedError)
 			t.Run(testName, func(t *testing.T) {
 				t.Parallel()
-
+				db := NewTestDB()
 				val, err := db.Get(testCase.key)
 				assert.Equal(t, testCase.expectedError, err)
 				assert.Equal(t, testCase.expectedValue, val)
@@ -59,29 +62,26 @@ func TestSet(t *testing.T) {
 		expectedLengthValues int
 	}{
 		{
-			key:                  "foo",
-			value:                []byte("bar"),
+			key:                  "exist",
+			value:                []byte("baz"),
 			expectedError:        nil,
 			expectedLengthValues: 1,
 		}, {
 			key:                  "foo",
 			value:                []byte("grid"),
 			expectedError:        nil,
-			expectedLengthValues: 1,
+			expectedLengthValues: 2,
 		},
 	}
 
 	t.Run("group", func(t *testing.T) {
-		db := &keyValue{
-			values: make(map[string][]byte),
-		}
-
 		for _, testCase := range testCases {
+
 			testCase := testCase
 			testName := fmt.Sprintf("key:%s/value:%v/length:%d/err:%v", testCase.key, testCase.value, testCase.expectedLengthValues, testCase.expectedError)
-
 			t.Run(testName, func(t *testing.T) {
 				t.Parallel()
+				db := NewTestDB()
 				err := db.Set(testCase.key, testCase.value)
 				assert.Equal(t, testCase.expectedError, err)
 				value := db.values[testCase.key]
@@ -101,29 +101,24 @@ func TestDelete(t *testing.T) {
 		expectedLengthValues int
 	}{
 		{
-			key:                  "exist-key",
+			key:                  "exist",
 			expectedError:        nil,
-			expectedLengthValues: 1,
+			expectedLengthValues: 0,
 		}, {
-			key:                  "not-exist-key",
+			key:                  "not-exist",
 			expectedError:        nil,
 			expectedLengthValues: 1,
 		},
 	}
 
 	t.Run("group", func(t *testing.T) {
-		db := &keyValue{
-			values: map[string][]byte{
-				"exist-key":   []byte("bar"),
-				"exist-key-1": []byte("foo"),
-			},
-		}
 		for _, testCase := range testCases {
+
 			testCase := testCase
 			testName := fmt.Sprintf("key:%s/length:%d/err:%v", testCase.key, testCase.expectedLengthValues, testCase.expectedError)
-
 			t.Run(testName, func(t *testing.T) {
 				t.Parallel()
+				db := NewTestDB()
 				err := db.Delete(testCase.key)
 				assert.Equal(t, testCase.expectedError, err)
 				assert.Equal(t, testCase.expectedLengthValues, len(db.values))
