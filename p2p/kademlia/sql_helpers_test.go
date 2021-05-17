8168f5e8a75b2285d44bb453e4ac603fa6092a6c
Alireza Bashiri <al3rez@gmail.com>
2021-05-17 23:25:11 +0700

    add test for sql_helper store

--- p2p/kademlia/sql_helpers_test.go
+++ p2p/kademlia/sql_helpers_test.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func TestMigrate(t *testing.T) {
+func Test_migrate(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
@@ -19,6 +19,24 @@ func TestMigrate(t *testing.T) {
 	}
 
 	if err := migrate(ctx, db); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_store(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	db, err := sql.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := migrate(ctx, db); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := store(db, []byte("sample key"), []byte("data"), time.Now(), time.Now()); err != nil {
 		t.Fatal(err)
 	}
 }
