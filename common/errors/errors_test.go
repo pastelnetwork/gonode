0353b16088eac8dde755360e4d640b0f3f2cc7ae
Ashadi <ashadi.cc@gmail.com>
2021-05-19 18:26:50 +0700

    errors package unit test

--- common/errors/errors_test.go
+++ common/errors/errors_test.go
@@ -1,27 +1,159 @@
 package errors_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/pastelnetwork/gonode/common/errors"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNew(t *testing.T) {
-	err := errors.New("foo")
-	assert.Equal(t, "foo", err.Error())
+func TestError(t *testing.T) {
+	t.Parallel()
 
-	err = errors.New("bar")
-	assert.Equal(t, "bar", err.Error())
+	testCases := []struct {
+		err      *errors.Error
+		expected string
+	}{
+		{
+			err:      errors.New("foo"),
+			expected: "foo",
+		}, {
+			err:      errors.New("bar"),
+			expected: "bar",
+		},
+	}
+
+	for i, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(fmt.Sprintf("testCase-%d", i), func(t *testing.T) {
+			assert.Equal(t, testCase.expected, testCase.err.Error())
+		})
+	}
 }
 
 func TestErrorf(t *testing.T) {
-	err := errors.Errorf("foo")
-	assert.Equal(t, "foo", err.Error())
+	t.Parallel()
+
+	testCases := []struct {
+		err      *errors.Error
+		expected string
+	}{
+		{
+			err:      errors.Errorf("foo: %s", "bar"),
+			expected: "foo: bar",
+		}, {
+			err:      errors.Errorf("foo: %t", true),
+			expected: "foo: true",
+		}, {
+			err:      errors.Errorf("foo: %d", 1),
+			expected: "foo: 1",
+		},
+	}
+
+	for i, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(fmt.Sprintf("testCase-%d", i), func(t *testing.T) {
+			assert.Equal(t, testCase.expected, testCase.err.Error())
+		})
+	}
+}
+
+func TestStack(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		err     *errors.Error
+		contain string
+	}{
+		{
+			err:     errors.New("stack trace"),
+			contain: "errors.New(\"stack trace\")",
+		},
+	}
+
+	for i, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(fmt.Sprintf("testCase-%d", i), func(t *testing.T) {
+			assert.Contains(t, testCase.err.Stack(), testCase.contain)
+		})
+	}
+}
+
+func TestUnwrap(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		err error
+	}{
+		{
+			err: fmt.Errorf("native error"),
+		},
+	}
+
+	for i, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(fmt.Sprintf("testCase-%d", i), func(t *testing.T) {
+			newError := errors.New(testCase.err)
+			assert.Equal(t, testCase.err, newError.Unwrap())
+		})
+	}
+
 }
 
 func TestErrorStack(t *testing.T) {
-	err := errors.Errorf("foo: %w", errors.New("bar"))
-	assert.Equal(t, "foo: bar", err.Error())
-	assert.Equal(t, err.Error()+"\n"+err.Stack(), err.ErrorStack())
+	t.Parallel()
+
+	testCases := []struct {
+		err      *errors.Error
+		expected string
+	}{
+		{
+			err:      errors.Errorf("foo: %w", errors.New("bar")),
+			expected: "foo: bar",
+		},
+	}
+
+	for i, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(fmt.Sprintf("testCase-%d", i), func(t *testing.T) {
+			assert.Equal(t, testCase.expected+"\n"+testCase.err.Stack(), testCase.err.ErrorStack())
+		})
+	}
+}
+
+func TestWithField(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		field string
+		value interface{}
+	}{
+		{
+			field: "foo",
+			value: "bar",
+		}, {
+			field: "bar",
+			value: "baz",
+		},
+	}
+
+	for i, testCase := range testCases {
+		testCase := testCase
+
+		t.Run(fmt.Sprintf("testCase-%d", i), func(t *testing.T) {
+			newError := errors.New("error").WithField(testCase.field, testCase.value)
+			fields := errors.ExtractFields(newError)
+
+			value, ok := fields[testCase.field]
+			assert.Equal(t, testCase.value, value)
+			assert.True(t, ok)
+
+		})
+	}
 }
