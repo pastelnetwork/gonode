be130cc41f42a031cfe5b1340eda173939bc5fcd
Alireza Bashiri <al3rez@gmail.com>
2021-05-25 03:01:56 +0700

    add missing tags for p2p/config

--- p2p/config.go
+++ p2p/config.go
@@ -1,7 +1,7 @@
 package p2p
 
 var (
-	defaultIP            = "0.0.0.0"
+	defaultListenAddress = "0.0.0.0"
 	defaultPort          = "0"
 	defaultBootstrapIP   = ""
 	defaultBootstrapPort = ""
@@ -11,25 +11,21 @@ var (
 // Config contains settings of the supernode server.
 type Config struct {
 	// The local IPv4 or IPv6 address
-	IP string
-
+	ListenAddress string `mapstructure:"listen_address" json:"listen_address,omitempty"`
 	// The local port to listen for connections on
-	Port string
-
+	Port string `mapstructure:"port" json:"port,omitempty"`
 	// IP Address to bootstrap against
-	BootstrapIP string
-
+	BootstrapIP string `mapstructure:"bootstrap_ip" json:"bootstrap_ip,omitempty"`
 	// Port to bootstrap against
-	BootstrapPort string
-
+	BootstrapPort string `mapstructure:"bootstrap_port" json:"bootstrap_port,omitempty"`
 	// Use STUN protocol for public addr discovery
-	UseStun bool
+	UseStun bool `mapstructure:"use_stun" json:"use_stun,omitempty"`
 }
 
 // NewConfig returns a new Config instance.
 func NewConfig() *Config {
 	return &Config{
-		IP:            defaultIP,
+		ListenAddress: defaultListenAddress,
 		Port:          defaultPort,
 		BootstrapIP:   defaultBootstrapIP,
 		BootstrapPort: defaultBootstrapPort,
