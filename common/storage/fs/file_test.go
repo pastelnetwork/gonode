9417eb7dd041cc0b47fe158bb19ecc7ff8dc3e18
ashadi <ashadi.cc@gmail.com>
2021-05-27 10:11:58 +0700

    init file_test

---
+++ common/storage/fs/file_test.go
@@ -0,0 +1,159 @@
+package fs
+
+import (
+	"testing"
+
+	"github.com/pastelnetwork/gonode/common/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFSOpen(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		dir string
+	}
+	type args struct {
+		filename string
+	}
+
+	type handleFunc func(t assert.TestingT)
+
+	tests := []struct {
+		name        string
+		fields      fields
+		args        args
+		createfunc  handleFunc
+		removeFunc  handleFunc
+		assertion   assert.ErrorAssertionFunc
+		valueAssert assert.ValueAssertionFunc
+	}{
+		{
+			name:        "valid open file",
+			fields:      fields{"./"},
+			args:        args{"test.txt"},
+			assertion:   assert.NoError,
+			valueAssert: assert.NotNil,
+			createfunc: func(t assert.TestingT) {
+				fs := &FS{
+					dir: "./",
+				}
+
+				_, err := fs.Create("test.txt")
+				assert.NoError(t, err)
+			},
+			removeFunc: func(t assert.TestingT) {
+				fs := &FS{
+					dir: "./",
+				}
+
+				assert.NoError(t, fs.Remove("test.txt"))
+			},
+		}, {
+			name:        "invalid open file",
+			fields:      fields{"./"},
+			args:        args{"non-exit.txt"},
+			assertion:   assert.Error,
+			valueAssert: assert.Nil,
+			createfunc:  func(t assert.TestingT) {},
+			removeFunc:  func(t assert.TestingT) {},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run("group", func(t *testing.T) {
+			tt.createfunc(t)
+
+			defer tt.removeFunc(t)
+
+			t.Run(tt.name, func(t *testing.T) {
+				fs := &FS{dir: tt.fields.dir}
+
+				got, err := fs.Open(tt.args.filename)
+				tt.assertion(t, err)
+				tt.valueAssert(t, got)
+			})
+		})
+
+	}
+}
+
+func TestFSCreate(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		dir string
+	}
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		want      storage.File
+		assertion assert.ErrorAssertionFunc
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fs := &FS{
+				dir: tt.fields.dir,
+			}
+			got, err := fs.Create(tt.args.filename)
+			tt.assertion(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestFSRemove(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		dir string
+	}
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		assertion assert.ErrorAssertionFunc
+	}{
+		// TODO: Add test cases.
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fs := &FS{
+				dir: tt.fields.dir,
+			}
+			tt.assertion(t, fs.Remove(tt.args.filename))
+		})
+	}
+}
+
+func TestNewFileStorage(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		dir string
+	}
+	tests := []struct {
+		name string
+		args args
+		want storage.FileStorage
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, NewFileStorage(tt.args.dir))
+		})
+	}
+}
