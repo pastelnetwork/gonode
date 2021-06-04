5b2b5a5f08d4e4c4c7d41a2dccbb52a6b2e36832
ashadi <ashadi.cc@gmail.com>
2021-06-04 09:34:11 +0700

    update task mock

--- common/service/task/test/mock_task.go
+++ common/service/task/test/mock_task.go
@@ -23,6 +23,24 @@ func (t *Task) ListenOnID(id string) *Task {
 	return t
 }
 
+// ListenOnRun listening Run call and returns error from args
+func (t *Task) ListenOnRun(returnErr error) *Task {
+	t.TaskMock.On("Run", mock.Anything).Return(returnErr)
+	return t
+}
+
+// ListenOnCancel listening Cancel call
+func (t *Task) ListenOnCancel() *Task {
+	t.TaskMock.On("Cancel").Return(nil)
+	return t
+}
+
+// ListenOnRunAction listening RunAction call and returns error from args
+func (t *Task) ListenOnRunAction(returnErr error) *Task {
+	t.TaskMock.On("RunAction", mock.Anything).Return(returnErr)
+	return t
+}
+
 // ListenOnUpdateStatus listening UpdateStatus call
 func (t *Task) ListenOnUpdateStatus() *Task {
 	t.TaskMock.On("UpdateStatus", mock.Anything).Return(nil)
