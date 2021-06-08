79bca0bd0acb87071ecf576529a761285c31545f
Sakib Sami <s4kibs4mi@gmail.com>
2021-06-09 01:39:58 +0600

    - changed file type conf to env

--- tools/pastel-utility/configs/init.go
+++ tools/pastel-utility/configs/init.go
@@ -1,11 +1,18 @@
 package configs
 
+import "encoding/json"
+
 // Init contains config of the Init command
 type Init struct {
 	WorkingDir string `json:"workdir,omitempty"`
 	Network    string `json:"network,omitempty"`
 	Force      bool   `json:"force,omitempty"`
 	Peers      string `json:"peers"`
+}
+
+func (i *Init) String() string {
+	b, _ := json.Marshal(i)
+	return string(b)
 }
 
 // NewInit returns a new Init instance.
