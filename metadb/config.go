feacdada0d287add25e6c326ebd4fbbbd9b0cc90
longwu <alonlong@live.cn>
2021-06-08 18:24:22 +0800

    Update metadb/config.go
    Co-authored-by: levkoburburas <62853952+levkoburburas@users.noreply.github.com>

--- metadb/config.go
+++ metadb/config.go
@@ -8,20 +8,17 @@ const (
 
 // Config contains settings of the rqlite server
 type Config struct {
-	// http server bind address. For HTTPS, set X.509 cert and key
-	HTTPAddress string `mapstructure:"http_address" json:"http_address,omitempty"`
-	// unique name for node. If not set, set to hostname
-	NodeID string `mapstructure:"node_id" json:"node_id,omitempty"`
-	// raft communication bind address
-	RaftAddress string `mapstructure:"raft_address" json:"raft_address,omitempty"`
-	// comma-delimited list of nodes, through which a cluster can be joined (proto://host:port)"
-	JoinAddress string `mapstructure:"join_address" json:"join_address,omitempty"`
-	// set Discovery Service URL
-	DiscoveryURL string `mapstructure:"discovery_url" json:"discovery_url,omitempty"`
-	// set Discovery ID. If not set, Discovery Service not used
-	DiscoveryID string `mapstructure:"discovery_id" json:"discovery_id,omitempty"`
+         // IPv4 or IPv6 address for listening by http server and raft
+	ListenAddress string `mapstructure:"listen_address" json:"listen_address,omitempty"`
+	
+	// http server bind port. For HTTPS, set X.509 cert and key
+	HTTPPort string `mapstructure:"http_port" json:"http_port,omitempty"`
+	
+	// raft port communication bind address
+	RaftPort string `mapstructure:"raft_port" json:"raft_port,omitempty"`
+
 	// data directory for rqlite
-	DataDir string `mapstructure:"data_dir" json:"data_dir,omitempty"`
+	DataDir string `mapstructure:"data_dir" json:"data_dir,omitempty"`		
 }
 
 // NewConfig returns a new Config instance.
