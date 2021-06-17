8e0b6c20a0c8a03a353293ef421e61e1ca866335
Matee ullah <matee.u@hotmail.com>
2021-06-17 15:27:07 +0500

    add p2p mock test client

---
+++ p2p/test/mock_client.go
@@ -0,0 +1,42 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/pastelnetwork/gonode/p2p/mocks"
+	"github.com/stretchr/testify/mock"
+)
+
+const (
+	// GetMethod represent Get name method
+	GetMethod = "Get"
+
+	// StoreMethod represent Store name method
+	StoreMethod = "Store"
+)
+
+// Client implementing pastel.Client for testing purpose
+type Client struct {
+	t *testing.T
+	*mocks.Client
+}
+
+// NewMockClient new Client instance
+func NewMockClient(t *testing.T) *Client {
+	return &Client{
+		t:      t,
+		Client: &mocks.Client{},
+	}
+}
+
+// ListenOnGet listening Get and returns data, found flag & err from args
+func (client *Client) ListenOnGet(data []byte, found bool, err error) *Client {
+	client.On(GetMethod, mock.Anything, mock.Anything).Return(data, found, err)
+	return client
+}
+
+// ListenOnStore listening Store and returns id & error from args
+func (client *Client) ListenOnStore(id string, err error) *Client {
+	client.On(StoreMethod, mock.Anything, mock.Anything).Return(id, err)
+	return client
+}
