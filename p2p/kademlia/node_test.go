e7fc7429e800fdd1ffc25dc570e2387f15a2e544
alonlong <alonlong@163.com>
2021-06-20 23:02:49 +0800

    add unit tests for Node

--- p2p/kademlia/node_test.go
+++ p2p/kademlia/node_test.go
@@ -41,3 +41,65 @@ func (s *testSuite) TestHasBit() {
 		s.Equal(true, hasBit(byte(255), uint(i)))
 	}
 }
+
+func (s *testSuite) newNodeList(n int) (*NodeList, error) {
+	nl := &NodeList{}
+
+	for i := 0; i < n; i++ {
+		id, err := newRandomID()
+		if err != nil {
+			return nil, err
+		}
+		nl.AddNodes([]*Node{
+			{
+				ID:   id,
+				IP:   s.IP,
+				Port: s.bootstrapPort + i + 1,
+			},
+		})
+	}
+	return nl, nil
+}
+
+func (s *testSuite) TestAddNodes() {
+	number := 5
+
+	nl, err := s.newNodeList(number)
+	if err != nil {
+		s.T().Fatalf("new node list: %v", err)
+	}
+	s.Equal(number, nl.Len())
+}
+
+func (s *testSuite) TestDelNodes() {
+	number := 5
+
+	nl, err := s.newNodeList(number)
+	if err != nil {
+		s.T().Fatalf("new node list: %v", err)
+	}
+	s.Equal(number, nl.Len())
+
+	ids := [][]byte{}
+	for _, node := range nl.Nodes {
+		ids = append(ids, node.ID)
+	}
+	for _, id := range ids {
+		nl.DelNode(&Node{ID: id})
+	}
+	s.Equal(0, nl.Len())
+}
+
+func (s *testSuite) TestExists() {
+	number := 5
+
+	nl, err := s.newNodeList(number)
+	if err != nil {
+		s.T().Fatalf("new node list: %v", err)
+	}
+	s.Equal(number, nl.Len())
+
+	for _, node := range nl.Nodes {
+		s.Equal(true, nl.Exists(node))
+	}
+}
