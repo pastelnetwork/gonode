5623a741c27c71a26c84ed9963729e0d83dd9d97
alonlong <alonlong@163.com>
2021-06-24 12:52:15 +0800

    make few changes to aes test

--- common/net/conn/secret/aes_test.go
+++ common/net/conn/secret/aes_test.go
@@ -9,12 +9,12 @@ import (
 
 func TestEncryptAndDecrypt(t *testing.T) {
 	// generate a random 32 byte key for AES-256
-	key := make([]byte, 64)
+	key := make([]byte, 32)
 	rand.Read(key)
 
-	key = key[:32]
 	source := make([]byte, 256*256)
 	rand.Read(source)
+
 	encrypted, err := aesEncrypt(source, key)
 	if err != nil {
 		t.Fatalf("aes encrypt: %v", err)
@@ -23,6 +23,5 @@ func TestEncryptAndDecrypt(t *testing.T) {
 	if err != nil {
 		t.Fatalf("aes decrypt: %v", err)
 	}
-
 	assert.Equal(t, source, decrypted)
 }
