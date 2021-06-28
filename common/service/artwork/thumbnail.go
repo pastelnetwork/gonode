96f34b3ffa20ef96ec7e6115154c4139ec84ffa7
leonidcode20 <leonidcode20@gmail.com>
2021-06-28 22:47:42 +0700

    Using qrdecoder to decode received image

---
+++ common/service/artwork/thumbnail.go
@@ -0,0 +1,8 @@
+package artwork
+
+type ImageThumbnail struct {
+	TopLeftX     int64 `json:"top_left_x"`
+	TopLeftY     int64 `json:"top_left_y"`
+	BottomRightX int64 `json:"bottom_right_x"`
+	BottomRightY int64 `json:"bottom_right_y"`
+}
