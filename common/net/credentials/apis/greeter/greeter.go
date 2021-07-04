2836cb5bbabf93abe639bf412b32a67e8d81c672
alonlong <alonlong@163.com>
2021-07-05 07:20:44 +0800

    fix the issues from circle ci

--- common/net/credentials/apis/greeter/greeter.go
+++ common/net/credentials/apis/greeter/greeter.go
@@ -2,10 +2,12 @@ package greeter
 
 import context "context"
 
+// Greeter implentation
 type Greeter struct {
 	UnimplementedGreeterServiceServer
 }
 
+// SayHi implements the api of Greeter service
 func (s *Greeter) SayHi(_ context.Context, request *SayHiRequest) (*SayHiReply, error) {
 	return &SayHiReply{Message: request.Name}, nil
 }
