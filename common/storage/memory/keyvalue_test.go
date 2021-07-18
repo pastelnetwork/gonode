18310cf714bd6443b640a959d76301883fbfc341
ashadi-cc <ashadi.cc@gmail.com>
2021-07-18 23:36:01 +0700

    Common/unit tests (#99)
    * storage:file unit test
    * refactor file_test
    * add worker unit test

--- common/storage/memory/keyvalue_test.go
+++ common/storage/memory/keyvalue_test.go
@@ -8,8 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// newTestDB. need keep as private method to prevent CI error:
-// exported func NewTestDB returns unexported type *memory.keyValue, which can be annoying to use
 func newTestDB() *keyValue {
 	return &keyValue{
 		values: map[string][]byte{
@@ -37,21 +35,21 @@ func TestGet(t *testing.T) {
 		},
 	}
 
-	t.Run("group", func(t *testing.T) {
-		for _, testCase := range testCases {
+	for _, testCase := range testCases {
+		testCase := testCase
 
-			testCase := testCase
-			testName := fmt.Sprintf("key:%s/value:%v/err:%v", testCase.key, testCase.expectedValue, testCase.expectedError)
-			t.Run(testName, func(t *testing.T) {
-				t.Parallel()
-				db := newTestDB()
-				val, err := db.Get(testCase.key)
-				assert.Equal(t, testCase.expectedError, err)
-				assert.Equal(t, testCase.expectedValue, val)
-			})
-		}
+		testName := fmt.Sprintf("key:%s/value:%v/err:%v", testCase.key, testCase.expectedValue, testCase.expectedError)
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
 
-	})
+			db := newTestDB()
+
+			val, err := db.Get(testCase.key)
+			assert.Equal(t, testCase.expectedError, err)
+			assert.Equal(t, testCase.expectedValue, val)
+		})
+	}
+
 }
 
 func TestSet(t *testing.T) {
@@ -73,22 +71,24 @@ func TestSet(t *testing.T) {
 		},
 	}
 
-	t.Run("group", func(t *testing.T) {
-		for _, testCase := range testCases {
+	for _, testCase := range testCases {
+		testCase := testCase
 
-			testCase := testCase
-			testName := fmt.Sprintf("key:%s/value:%v/err:%v", testCase.key, testCase.value, testCase.expectedError)
-			t.Run(testName, func(t *testing.T) {
-				t.Parallel()
-				db := newTestDB()
-				err := db.Set(testCase.key, testCase.value)
-				assert.Equal(t, testCase.expectedError, err)
-				value, ok := db.values[testCase.key]
-				assert.True(t, ok, "not found new key")
-				assert.Equal(t, testCase.value, value)
-			})
-		}
-	})
+		testName := fmt.Sprintf("key:%s/value:%v/err:%v", testCase.key, testCase.value, testCase.expectedError)
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+
+			db := newTestDB()
+
+			err := db.Set(testCase.key, testCase.value)
+			assert.Equal(t, testCase.expectedError, err)
+
+			value, ok := db.values[testCase.key]
+			assert.True(t, ok, "not found new key")
+			assert.Equal(t, testCase.value, value)
+		})
+	}
+
 }
 
 func TestDelete(t *testing.T) {
@@ -107,19 +107,20 @@ func TestDelete(t *testing.T) {
 		},
 	}
 
-	t.Run("group", func(t *testing.T) {
-		for _, testCase := range testCases {
+	for _, testCase := range testCases {
+		testCase := testCase
 
-			testCase := testCase
-			testName := fmt.Sprintf("key:%s/err:%v", testCase.key, testCase.expectedError)
-			t.Run(testName, func(t *testing.T) {
-				t.Parallel()
-				db := newTestDB()
-				err := db.Delete(testCase.key)
-				_, ok := db.values[testCase.key]
-				assert.Equal(t, testCase.expectedError, err)
-				assert.False(t, ok, "found deleted key")
-			})
-		}
-	})
+		testName := fmt.Sprintf("key:%s/err:%v", testCase.key, testCase.expectedError)
+		t.Run(testName, func(t *testing.T) {
+			t.Parallel()
+
+			db := newTestDB()
+
+			err := db.Delete(testCase.key)
+			assert.Equal(t, testCase.expectedError, err)
+
+			_, ok := db.values[testCase.key]
+			assert.False(t, ok, "found deleted key")
+		})
+	}
 }
