7a5041c2728f7a2593d1a04ffef758a7c69955a8
pjenys0 <pjenys.dev0@gmail.com>
2021-07-21 21:23:08 +0700

    update api

--- raptorq/test/test.go
+++ raptorq/test/test.go
@@ -5,14 +5,16 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/pastelnetwork/gonode/raptorq/node"
 	"github.com/pastelnetwork/gonode/raptorq/node/grpc"
 )
 
 func main() {
 	// create connection to service
 	client := grpc.NewClient()
-	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 	defer cancel()
+
 	connection, err := client.Connect(ctx, "127.0.0.1:50051")
 	if err != nil {
 		fmt.Printf("Connect error:%v\n", err)
@@ -21,17 +23,26 @@ func main() {
 
 	data := []byte("ababcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdcd")
 
-	service := connection.RaptorQ()
-	res, err := service.Encode(context.Background(), data)
+	service := connection.RaptorQ(&node.Config{
+		RqFilesDir: "/tmp/",
+	})
 
+	res1, err := service.Encode(context.Background(), data)
 	if err != nil {
 		fmt.Printf("Encode error:%v\n", err)
-
 		return
 	}
 
-	// print result
-	for i, _ := range res {
-		fmt.Println(string(res[i]))
+	fmt.Println("Encode Results:")
+	fmt.Println(res1)
+
+	res2, err := service.EncodeInfo(context.Background(), data, 2, "block_hash", "pastelID")
+
+	if err != nil {
+		fmt.Printf("EncodeInfo error:%v\n", err)
+		return
 	}
+	fmt.Println("EncodeInfo Results:")
+	fmt.Println(res2)
+
 }
