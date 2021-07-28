453ed2c8ebc1158a67876e279eb48a8298eecf2b
georgkcode0 <georgk.code0@gmail.com>
2021-07-28 20:01:56 +0700

    add challenge number and rekey for xchacha20poly1305ietf

--- common/net/credentials/alts/conn/xchacha20poly1305ietf.go
+++ common/net/credentials/alts/conn/xchacha20poly1305ietf.go
@@ -1,8 +1,6 @@
 package conn
 
 import (
-	"fmt"
-
 	"github.com/GoKillers/libsodium-go/crypto/aead/xchacha20poly1305ietf"
 	"github.com/pastelnetwork/gonode/common/net/credentials/alts"
 )
@@ -13,60 +11,67 @@ const (
 	overflowLenxChacha20Poly1305IETF = 8
 )
 
-type _xchacha20poly1305ietf struct {
-	key        [xchacha20poly1305ietf.KeyBytes]byte
-	nonce      [xchacha20poly1305ietf.NonceBytes]byte
+type xchacha20poly1305ietfReKey struct {
 	inCounter  Counter
 	outCounter Counter
+	inAEAD     *rekeyAEADChaChaPoly
+	outAEAD    *rekeyAEADChaChaPoly
 }
 
-// NewxChaCha20Poly1305IETF creates an instance that uses chacha20poly1305 with rekeying
+// NewxChaCha20Poly1305IETFReKey creates an instance that uses chacha20poly1305 with rekeying
 // for ALTS record. The key argument should be 56 bytes, the first 32 bytes are used as
 // a secret key and the remainining 24 bytes are used as public nonce
 // Refer to: https://doc.libsodium.org/secret-key_cryptography/aead/chacha20-poly1305/xchacha20-poly1305_construction
-func NewxChaCha20Poly1305IETF(side alts.Side, key []byte) (ALTSRecordCrypto, error) {
-	k := len(key)
-	if k != xchacha20poly1305ietf.KeyBytes+xchacha20poly1305ietf.NonceBytes {
-		return nil, KeySizeError(k)
-	}
+func NewxChaCha20Poly1305IETFReKey(side alts.Side, key []byte) (ALTSRecordCrypto, error) {
 	inCounter := NewInCounter(side, overflowLenxChacha20Poly1305IETF)
 	outCounter := NewOutCounter(side, overflowLenxChacha20Poly1305IETF)
 
-	c := &_xchacha20poly1305ietf{
+	inAEAD, err := newRekeyAEADChaChaPoly(key)
+	if err != nil {
+		return nil, err
+	}
+	outAEAD, err := newRekeyAEADChaChaPoly(key)
+	if err != nil {
+		return nil, err
+	}
+
+	return &xchacha20poly1305ietfReKey{
 		inCounter:  inCounter,
 		outCounter: outCounter,
-	}
-	copy(c.key[:], key[:xchacha20poly1305ietf.KeyBytes])
-	copy(c.nonce[:], key[xchacha20poly1305ietf.KeyBytes:])
-	return c, nil
+		inAEAD:     inAEAD,
+		outAEAD:    outAEAD,
+	}, nil
 }
 
-func (s *_xchacha20poly1305ietf) Encrypt(dst, plaintext []byte) ([]byte, error) {
+func (s *xchacha20poly1305ietfReKey) Encrypt(dst, plaintext []byte) ([]byte, error) {
 	seq, err := s.outCounter.Value()
 	if err != nil {
 		return nil, err
 	}
 
-	ec := xchacha20poly1305ietf.Encrypt(plaintext, seq, &s.nonce, &s.key)
+	ec, err := s.outAEAD.Encrypt(plaintext, seq)
+	if err != nil {
+		return nil, err
+	}
 	dst = append(dst, ec...)
 	s.outCounter.Inc()
 	return dst, nil
 }
 
 // EncryptionOverhead returns tag size
-func (s *_xchacha20poly1305ietf) EncryptionOverhead() int {
+func (s *xchacha20poly1305ietfReKey) EncryptionOverhead() int {
 	return xchacha20poly1305ietf.ABytes
 }
 
-func (s *_xchacha20poly1305ietf) Decrypt(dst, ciphertext []byte) ([]byte, error) {
+func (s *xchacha20poly1305ietfReKey) Decrypt(dst, ciphertext []byte) ([]byte, error) {
 	seq, err := s.inCounter.Value()
 	if err != nil {
 		return nil, err
 	}
 
-	p, err := xchacha20poly1305ietf.Decrypt(ciphertext, seq, &s.nonce, &s.key)
+	p, err := s.inAEAD.Decrypt(ciphertext, seq)
 	if err != nil {
-		return nil, fmt.Errorf("decryption unexpectedly succeeded for %s", err)
+		return nil, err
 	}
 	s.inCounter.Inc()
 	return p, nil
