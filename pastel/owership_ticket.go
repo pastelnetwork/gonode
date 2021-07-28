d48a789ba45785d4d31944db61dee1a980aed1b4
danileng20 <danil.engcode20@gmail.com>
2021-07-29 00:08:25 +0700

    Remove unused file.

--- pastel/owership_ticket.go
+++
@@ -1,7 +0,0 @@
-package pastel
-
-// OwnershipTicket represents ticket's ownership
-type OwnershipTicket struct {
-	Art   string `json:"art"`   // txid from the request
-	Trade string `json:"trade"` // txid from trade ticket
-}
