073cca593e873079a3d7f15a2851eb763e17dcf6
leonidcode20 <leonidcode20@gmail.com>
2021-08-22 22:06:11 +0700

    Generate goa again without manual adding 'defer cancel()'

--- walletnode/api/gen/http/artworks/client/client.go
+++ walletnode/api/gen/http/artworks/client/client.go
@@ -143,7 +143,6 @@ func (c *Client) RegisterTaskState() goa.Endpoint {
 		}
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithCancel(ctx)
-		defer cancel()
 		conn, resp, err := c.dialer.DialContext(ctx, req.URL.String(), req.Header)
 		if err != nil {
 			if resp != nil {
@@ -248,7 +247,6 @@ func (c *Client) ArtSearch() goa.Endpoint {
 		}
 		var cancel context.CancelFunc
 		ctx, cancel = context.WithCancel(ctx)
-		defer cancel()
 		conn, resp, err := c.dialer.DialContext(ctx, req.URL.String(), req.Header)
 		if err != nil {
 			if resp != nil {
