746ebbf603676ec97d68c265b530316af0139009
Matee ullah <matee.u@hotmail.com>
2021-08-27 05:55:20 +0500

    create and hash thumbnail test

--- supernode/services/artworkregister/task_thumbnail_test.go
+++ supernode/services/artworkregister/task_thumbnail_test.go
@@ -2,8 +2,16 @@ package artworkregister
 
 import (
 	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"strings"
 	"testing"
 
+	"github.com/pastelnetwork/gonode/common/errors"
+	"github.com/pastelnetwork/gonode/common/service/artwork"
+	"github.com/pastelnetwork/gonode/common/storage/fs"
+	"github.com/pastelnetwork/gonode/pastel"
 	"github.com/tj/assert"
 )
 
@@ -111,6 +119,89 @@ func TestMaxInt(t *testing.T) {
 			t.Parallel()
 			got := maxInt(tc.x, tc.y)
 			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func newTestImageFile(stg *artwork.Storage) (*artwork.File, error) {
+	imgFile := stg.NewFile()
+
+	f, err := imgFile.Create()
+	if err != nil {
+		return nil, errors.Errorf("failed to create storage file: %w", err)
+	}
+	defer f.Close()
+
+	img := image.NewRGBA(image.Rect(0, 0, 400, 400))
+	if err := png.Encode(f, img); err != nil {
+		return nil, err
+	}
+
+	return imgFile, nil
+}
+func TestCreateAndHashThumbnail(t *testing.T) {
+	type args struct {
+		task     *Task
+		storeErr error
+		fileErr  error
+	}
+
+	testCases := map[string]struct {
+		args    args
+		wantErr error
+	}{
+		"success": {
+			args: args{
+				task: &Task{
+					Service: &Service{
+						config: &Config{},
+					},
+					Ticket: &pastel.NFTTicket{},
+				},
+				fileErr:  nil,
+				storeErr: nil,
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+
+		t.Run(fmt.Sprintf("testCase-%v", name), func(t *testing.T) {
+			t.Parallel()
+
+			stg := artwork.NewStorage(fs.NewFileStorage(os.TempDir()))
+
+			tc.args.task.Storage = stg
+			file, err := newTestImageFile(stg)
+			assert.Nil(t, err)
+			tc.args.task.Artwork = file
+
+			srcImg, err := tc.args.task.Artwork.LoadImage()
+			if err != nil {
+				fmt.Println("errrr: ", err.Error())
+			}
+			assert.Nil(t, err)
+
+			originalW := srcImg.Bounds().Dx()
+			originalH := srcImg.Bounds().Dy()
+			// determine quality for preview thumbnail
+			previewQuality := determinePreviewQuality(maxInt(originalW, originalH))
+
+			_, err = tc.args.task.createAndHashThumbnail(srcImg, previewThumbnail, nil, previewQuality, fileSizeLimit)
+			if tc.wantErr != nil {
+				if err != nil {
+					fmt.Println("errrr: ", err.Error())
+				}
+				assert.NotNil(t, err)
+				assert.True(t, strings.Contains(err.Error(), tc.wantErr.Error()))
+			} else {
+				if err != nil {
+					fmt.Println("errrrr: ", err.Error())
+				}
+				assert.Nil(t, err)
+			}
 		})
 	}
 }
