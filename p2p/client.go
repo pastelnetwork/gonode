bfa359df55e73efc90061e4ad710084df086dbcc
nguyen202 <nguyen202v@gmail.com>
2021-09-06 21:06:50 +0700

    Remove not-used struct

--- p2p/client.go
+++ p2p/client.go
@@ -4,13 +4,6 @@ package p2p
 
 import "context"
 
-// return current status of backend Database
-type DbStatus struct {
-	SizeInMb int // Current size of DB
-	KeyCount int // number of keys in the table
-	//.. others put here
-}
-
 // Client exposes the interfaces for p2p service
 type Client interface {
 	// Retrieve data from the kademlia network by key, return nil if not found
