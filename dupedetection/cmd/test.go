769c1a7d3e9d1bdb1ce0fdc740e084c5b648a796
nguyen202 <nguyen202v@gmail.com>
2021-09-25 21:29:59 +0700

    fix unit test

--- dupedetection/cmd/test.go
+++ dupedetection/cmd/test.go
@@ -1,70 +1,30 @@
 package main
 
-func main() {
-
-}
-
-/*
 import (
 	"context"
-	"flag"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/pastelnetwork/gonode/dupedetection"
-)
-
-const (
-	defaultTestImage = "test.png"
+	"github.com/pastelnetwork/gonode/dupedetection/ddclient"
 )
 
 func main() {
-	fmt.Println("Start dupe detection test...")
-	defer fmt.Println("Finished dupe detection test.")
-
-	workDir := flag.String("workDir", "", "a path to the working directory.")
-	flag.Parse()
-
-	currentDir, _ := os.Getwd()
-	if *workDir != "" {
-		currentDir = *workDir
+	config := &ddclient.Config{
+		Host:       "localhost",
+		Port:       50052,
+		DDFilesDir: "/tmp",
 	}
-
-	ddconf := dupedetection.NewConfig()
-	ddconf.SetWorkDir(currentDir)
-
-	fmt.Printf("input directory: %s\n", ddconf.InputDir)
-	fmt.Printf("output directory: %s\n", ddconf.OutputDir)
-	client := dupedetection.NewClient(ddconf)
-
-	// read image content
-	img, err := ioutil.ReadFile(defaultTestImage)
+	client := ddclient.NewDDServerClient(config)
+	data, err := ioutil.ReadFile("test.png")
 	if err != nil {
-		fmt.Printf("Could not read image content: %v\n", err)
-		return
+		panic("read file error" + err.Error())
 	}
 
-	format := filepath.Ext(defaultTestImage)
-
-	ctx := context.Background()
-	result, err := client.Generate(ctx, img, strings.TrimPrefix(format, "."))
+	_, err = client.ImageRarenessScore(context.Background(), data, "png")
 	if err != nil {
-		fmt.Printf("Could not get fringerprints from dupe detection service: %v\n", err)
-		return
+		panic("do ImageRarenessScore() error" + err.Error())
 	}
-	fmt.Printf("Dupe detection system version: %s\n", result.DupeDetectionSystemVer)
-	fmt.Printf("Image hash: %s\n", result.ImageHash)
-	fmt.Printf("Pastel Rareness Score: %v\n", result.PastelRarenessScore)
-	fmt.Printf("Internet Rareness Score: %v\n", result.InternetRarenessScore)
-	fmt.Printf("Open NSFW Score: %v\n", result.OpenNSFWScore)
-	fmt.Printf("Matches found on first page: %v\n", result.MatchesFoundOnFirstPage)
-	fmt.Printf("Number of result pages: %v\n", result.NumberOfResultPages)
-	fmt.Printf("First match URL: %s\n", result.FirstMatchURL)
-	fmt.Printf("Alternate NSFW Scores: %v\n", result.AlternateNSFWScores)
-	fmt.Printf("Image Hashes: %v\n", result.ImageHashes)
-	fmt.Printf("Fingerprints: %v\n", result.Fingerprints)
+
+	fmt.Println("Successful")
+
 }
-*/
