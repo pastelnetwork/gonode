bd75931e5cb43e501d06436fc6bd9fd37c42c79f
dnvu92 <dnvu92h@gmail.com>
2022-01-03 09:56:01 +0700

    update

--- supernode/services/storagechallenge/repository.go
+++ supernode/services/storagechallenge/repository.go
@@ -8,6 +8,14 @@ import (
 	"github.com/pastelnetwork/gonode/pastel"
 )
 
+type SaveChallengState interface {
+	OnSent(ctx context.Context, challengeID, nodeID string, sentBlock int32)
+	OnResponded(ctx context.Context, challengeID, nodeID string, sentBlock int32)
+	OnSucceeded(ctx context.Context, challengeID, nodeID string, sentBlock int32)
+	OnFailed(ctx context.Context, challengeID, nodeID string, sentBlock int32)
+	OnTimeout(ctx context.Context, challengeID, nodeID string, sentBlock int32)
+}
+
 type repository interface {
 	// ListSymbolFileKeysFromNFTTicket func
 	ListSymbolFileKeysFromNFTTicket(ctx context.Context) ([]string, error)
@@ -25,18 +33,22 @@ type repository interface {
 	GetNClosestMasternodesToAGivenFileUsingKademlia(ctx context.Context, n int, comparisonString string, ignores ...string) []string
 	// GetNClosestFileHashesToAGivenComparisonString func
 	GetNClosestFileHashesToAGivenComparisonString(ctx context.Context, n int, comparisonString string, listFileHashes []string, ignores ...string) []string
+	// SaveChallengMessageState func
+	SaveChallengMessageState(ctx context.Context, status, challengeID, nodeID string, sentBlock int32)
 }
 
-func newRepository(p2p p2p.Client, pClient pastel.Client) repository {
+func newRepository(p2p p2p.Client, pClient pastel.Client, challengeStateStorage SaveChallengState) repository {
 	return &repo{
-		p2p:     p2p,
-		pClient: pClient,
+		p2p:          p2p,
+		pClient:      pClient,
+		stateStorage: challengeStateStorage,
 	}
 }
 
 type repo struct {
-	p2p     p2p.Client
-	pClient pastel.Client
+	p2p          p2p.Client
+	pClient      pastel.Client
+	stateStorage SaveChallengState
 }
 
 func (r *repo) ListSymbolFileKeysFromNFTTicket(ctx context.Context) ([]string, error) {
@@ -68,7 +80,7 @@ func (r *repo) RemoveSymbolFileByKey(ctx context.Context, key string) error {
 
 func (r *repo) GetListOfMasternode(ctx context.Context) ([]string, error) {
 	var ret = make([]string, 0)
-	listMN, err := r.pClient.MasterNodesList(ctx)
+	listMN, err := r.pClient.MasterNodesExtra(ctx)
 	if err != nil {
 		return ret, err
 	}
@@ -90,4 +102,22 @@ func (r *repo) GetNClosestMasternodesToAGivenFileUsingKademlia(ctx context.Conte
 
 func (r *repo) GetNClosestFileHashesToAGivenComparisonString(_ context.Context, n int, comparisonString string, listFileHashes []string, ignores ...string) []string {
 	return utils.GetNClosestXORDistanceStringToAGivenComparisonString(n, comparisonString, listFileHashes, ignores...)
+}
+
+func (r *repo) SaveChallengMessageState(ctx context.Context, status string, challengeID, nodeID string, sentBlock int32) {
+	var caller func(ctx context.Context, challengeID, nodeID string, sentBlock int32)
+	switch status {
+	case "sent":
+		caller = r.stateStorage.OnSent
+	case "responded":
+		caller = r.stateStorage.OnResponded
+	case "succeeded":
+		caller = r.stateStorage.OnSucceeded
+	case "failed":
+		caller = r.stateStorage.OnFailed
+	case "timeout":
+		caller = r.stateStorage.OnTimeout
+	}
+
+	caller(ctx, challengeID, nodeID, sentBlock)
 }
