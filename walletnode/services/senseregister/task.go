bd26c2817df801cc4a4703d5efc94509e97d7f7c
Matee <matee.u@hotmail.com>
2022-02-04 21:53:09 +0500

    fix build err & unit tests

--- walletnode/services/senseregister/task.go
+++ walletnode/services/senseregister/task.go
@@ -112,8 +112,9 @@ func (task *SenseRegistrationTask) run(ctx context.Context) error {
 	// new context because the old context already cancelled
 	newCtx := context.Background()
 	if err := task.service.pastelHandler.WaitTxidValid(newCtx, task.regSenseTxid, int64(task.service.config.SenseRegTxMinConfirmations),
-	 time.Duration(task.service.config.WaitTxnValidInterval)*time.Second); err != nil {
+		time.Duration(task.service.config.WaitTxnValidInterval)*time.Second); err != nil {
 		_ = task.MeshHandler.CloseSNsConnections(ctx, nodesDone)
+
 		return errors.Errorf("wait reg-nft ticket valid: %w", err)
 	}
 	task.UpdateStatus(common.StatusTicketRegistered)
