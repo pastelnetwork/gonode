f1b2fd813d025537ef84c94b6815b05a7ecc298a
Bjbrostoff <ben.brostoff@pastel.network>
2022-02-22 08:08:02 -0800

    WIP commit, need to swap branches, working on testing.

--- supernode/services/storagechallenge/task_test.go
+++ supernode/services/storagechallenge/task_test.go
@@ -2,8 +2,18 @@ package storagechallenge
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 
+	fuzz "github.com/google/gofuzz"
+	"github.com/pastelnetwork/gonode/common/b85"
+	storageMock "github.com/pastelnetwork/gonode/common/storage/test"
+	p2pMock "github.com/pastelnetwork/gonode/p2p/test"
+	"github.com/pastelnetwork/gonode/pastel"
+	pastelMock "github.com/pastelnetwork/gonode/pastel/test"
+	rqnode "github.com/pastelnetwork/gonode/raptorq/node"
+	rqMock "github.com/pastelnetwork/gonode/raptorq/node/test"
+	sctest "github.com/pastelnetwork/gonode/supernode/node/test/storage_challenge"
 	"github.com/pastelnetwork/gonode/supernode/services/common"
 )
 
@@ -29,11 +39,50 @@ func TestTaskGenerateStorageChallenges(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+
+			ticket := pastel.RegTicket{}
+			f := fuzz.New()
+			f.Fuzz(&ticket)
+			ticket.Height = 2
+
+			b, err := json.Marshal(ticket.RegTicketData.NFTTicketData.AppTicketData)
+			if err != nil {
+				t.Fatalf("faied to marshal, err: %s", err)
+			}
+			ticket.RegTicketData.NFTTicketData.AppTicket = b85.Encode(b)
+
+			b, err = json.Marshal(ticket.RegTicketData.NFTTicketData)
+			if err != nil {
+				t.Fatalf("faied to marshal, err: %s", err)
+			}
+			ticket.RegTicketData.NFTTicket = b
+
+			pMock := pastelMock.NewMockClient(t)
+			pMock.ListenOnRegTickets(pastel.RegTickets{
+				ticket,
+			}, nil).ListenOnGetBlockCount(1, nil).ListenOnGetBlockVerbose1(nil, nil)
+
+			p2pClientMock := p2pMock.NewMockClient(t).ListenOnRetrieve(nil, nil)
+
+			rqClientMock := rqMock.NewMockClient(t)
+			rqClientMock.ListenOnEncodeInfo(&rqnode.EncodeInfo{}, nil)
+			rqClientMock.ListenOnRaptorQ().ListenOnClose(nil)
+			// rqClientMock.ListenOnConnect(tt.args.connectErr)
+
+			clientMock := sctest.NewMockClient(t)
+			clientMock.ListenOnConnect("", nil)
+
+			fsMock := storageMock.NewMockFileStorage()
+			// storage := files.NewStorage(fsMock)
+
+			testConfig := NewConfig()
+
 			task := StorageChallengeTask{
 				SuperNodeTask:           tt.fields.SuperNodeTask,
-				StorageChallengeService: tt.fields.StorageChallengeService,
-				storage:                 tt.fields.storage,
+				StorageChallengeService: NewService(testConfig, fsMock, pMock.Client, clientMock, p2pClientMock, rqClientMock, nil),
+				storage:                 common.NewStorageHandler(p2pClientMock, rqClientMock, testConfig.RaptorQServiceAddress, testConfig.RqFilesDir),
 			}
+
 			if err := task.GenerateStorageChallenges(tt.args.ctx); (err != nil) != tt.wantErr {
 				t.Errorf("StorageChallengeTask.GenerateStorageChallenges() error = %v, wantErr %v", err, tt.wantErr)
 			}
