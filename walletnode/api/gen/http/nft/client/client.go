6bcc950208b9d07c775065a8d0bfeedab6119bce
Matee <matee.u@hotmail.com>
2022-06-10 05:24:17 +0500

    fix lint err

--- walletnode/api/gen/http/nft/client/client.go
+++ walletnode/api/gen/http/nft/client/client.go
@@ -146,7 +146,9 @@ func (c *Client) RegisterTaskState() goa.Endpoint {
 			return nil, err
 		}
 		var cancel context.CancelFunc
-		ctx, cancel = context.WithCancel(ctx)
+		_, cancel = context.WithCancel(ctx)
+		defer cancel()
+
 		conn, resp, err := c.dialer.DialContext(ctx, req.URL.String(), req.Header)
 		if err != nil {
 			if resp != nil {
@@ -269,7 +271,9 @@ func (c *Client) NftSearch() goa.Endpoint {
 			return nil, err
 		}
 		var cancel context.CancelFunc
-		ctx, cancel = context.WithCancel(ctx)
+		_, cancel = context.WithCancel(ctx)
+		defer cancel()
+
 		conn, resp, err := c.dialer.DialContext(ctx, req.URL.String(), req.Header)
 		if err != nil {
 			if resp != nil {
