75ac2c99a46b385a65f3b8a740f791cdafaedcdd
Matee <matee.u@hotmail.com>
2023-09-11 18:13:41 +0500

    fix retry

--- tools/check_db_mismatch/go.mod
+++ tools/check_db_mismatch/go.mod
@@ -2,4 +2,27 @@ module github.com/pastelnetwork/gonode/tools/check_db_mismatch
 
 go 1.21
 
-require github.com/mattn/go-sqlite3 v1.14.17
+replace github.com/pastelnetwork/gonode/common => ../../common
+
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/mattn/go-sqlite3 v1.14.17
+	github.com/pastelnetwork/gonode/common v0.0.0-00010101000000-000000000000
+)
+
+require (
+	github.com/go-errors/errors v1.4.2 // indirect
+	github.com/jpillora/longestcommon v0.0.0-20161227235612-adb9d91ee629 // indirect
+	github.com/klauspost/compress v1.16.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.10 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/x-cray/logrus-prefixed-formatter v0.5.2 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/term v0.10.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+)
--- tools/check_db_mismatch/main.go
+++ tools/check_db_mismatch/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-	"log"
+	"strings"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3" //go-sqlite3
+	"github.com/pastelnetwork/gonode/common/log"
+	"github.com/pastelnetwork/gonode/common/utils"
 )
 
-func fetchTxIDsFromDB(db *sql.DB) (map[string]bool, error) {
+func fetchTxIDsFromDB(db *sqlx.DB) (map[string]bool, error) {
 	txIDs := make(map[string]bool)
 
 	rows, err := db.Query("SELECT registration_ticket_txid FROM image_hash_to_image_fingerprint_table WHERE registration_ticket_txid IS NOT NULL")
@@ -29,16 +32,21 @@ func fetchTxIDsFromDB(db *sql.DB) (map[string]bool, error) {
 }
 
 func main() {
-	db1, err := sql.Open("sqlite3", "./hermes-154.12.235.16.sqlite")
+
+	db1, err := sqlx.Connect("sqlite3", "hermes-154.12.235.12.sqlite")
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf("cannot open dd-service database: %w", err)
+		return
 	}
+
 	defer db1.Close()
 
-	db2, err := sql.Open("sqlite3", "./hermes-154.12.235.41.sqlite")
+	db2, err := sqlx.Connect("sqlite3", "hermes-3.19.48.187.sqlite")
 	if err != nil {
-		log.Fatal(err)
+		log.Errorf("cannot open dd-service database: %w", err)
+		return
 	}
+
 	defer db2.Close()
 
 	txIDs1, err := fetchTxIDsFromDB(db1)
@@ -54,17 +62,68 @@ func main() {
 	}
 	fmt.Println("Number of txIDs in hermes 2", len(txIDs2))
 
-	fmt.Println("TxIDs in hermes-154.12.235.16.sqlite but not in hermes-154.12.235.41.sqlite:")
+	fmt.Println("TxIDs in hermes-154.12.235.16.sqlite but not in hermes-154.38.161.58.sqlite:")
 	for txID := range txIDs1 {
 		if _, exists := txIDs2[txID]; !exists {
 			fmt.Println(txID)
 		}
 	}
 
-	fmt.Println("\nTxIDs in hermes-154.12.235.41.sqlite but not in hermes-154.12.235.16.sqlite:")
+	fmt.Println("\nTxIDs in hermes-154.12.235.41.sqlite but not in hermes-154.12.235.41.sqlite:")
 	for txID := range txIDs2 {
 		if _, exists := txIDs1[txID]; !exists {
 			fmt.Println(txID)
 		}
 	}
+
+	hash, err := GetDDDataHash(context.Background(), db1)
+	if err != nil {
+		log.Errorf("cannot open dd-service database: %w", err)
+		return
+	}
+
+	hash2, err := GetDDDataHash(context.Background(), db2)
+	if err != nil {
+		log.Errorf("cannot open dd-service database: %w", err)
+		return
+	}
+
+	fmt.Println("hash1", hash)
+	fmt.Println("hash2", hash2)
+}
+
+func GetDDDataHash(ctx context.Context, db *sqlx.DB) (hash string, err error) {
+	r := []fingerprints{}
+	err = db.Select(&r, "SELECT sha256_hash_of_art_image_file FROM image_hash_to_image_fingerprint_table where txid_timestamp >0 order by sha256_hash_of_art_image_file asc")
+	if err != nil {
+		log.WithContext(ctx).WithError(err).Error("failed to get image_hash_to_image_fingerprint_table")
+	}
+
+	c := []collections{}
+	err = db.Select(&c, "SELECT collection_ticket_txid FROM collections_table")
+	if err != nil {
+		log.WithContext(ctx).WithError(err).Error("failed to get collections_table, ignore table")
+	}
+
+	var sb strings.Builder
+
+	for i := 0; i < len(r); i++ {
+		sb.WriteString(r[i].Sha256HashOfArtImageFile)
+	}
+
+	for i := 0; i < len(c); i++ {
+		sb.WriteString(c[i].CollectionTicketTXID)
+	}
+
+	hash = utils.GetHashFromString(sb.String())
+	log.WithContext(ctx).WithField("hash", hash).Debug("dd data hash returned")
+
+	return hash, nil
+}
+
+type fingerprints struct {
+	Sha256HashOfArtImageFile string `db:"sha256_hash_of_art_image_file,omitempty"`
+}
+type collections struct {
+	CollectionTicketTXID string `db:"collection_ticket_txid"`
 }
