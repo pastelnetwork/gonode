cc9e16636f6dcb2a8e28adebb6f83d415b04a9e7
j-rafique <mejrafique@gmail.com>
2023-11-22 15:18:27 +0500

    [PSL-1060] implement gRPC server for ping info worker

---
+++ supernode/services/selfhealing/process_ping.go
@@ -0,0 +1,20 @@
+package selfhealing
+
+import (
+	"context"
+	"time"
+
+	"github.com/pastelnetwork/gonode/common/log"
+	pb "github.com/pastelnetwork/gonode/proto/supernode"
+)
+
+// Ping acknowledges the received message and return with the timestamp
+func (task *SHTask) Ping(ctx context.Context, _ *pb.PingRequest) (*pb.PingResponse, error) {
+	log.WithContext(ctx).WithField("node_id", task.nodeID).Println("ping request received at the server")
+
+	return &pb.PingResponse{
+		ReceiverId:  task.nodeID,
+		IsOnline:    true,
+		RespondedAt: time.Now().UTC().String(),
+	}, nil
+}
